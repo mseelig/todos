1cbe3af5144c6281ffe7bfedaae197b7eb2fb9be
Mark Seelig <mark@fitorbit.com>
2012-09-24 11:42:22 -0700

    Add ability to create todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Todos::Application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'homepage'
   resource :session, only: [:create]
+  resources :todos, only: [:index, :new, :create]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
